25ccd539c19cc223b4dc5e74c55e272ac252ea94
ekhomid <87720921+ekhomid@users.noreply.github.com>
2022-04-22 05:52:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+/*
+Есть упорядоченная последовательность чисел от 1 до N. 
+Из нее удалили одно число, а оставшиеся перемешали. 
+Нужно написать функцию, которая по итоговому массиву находит удаленное число.
+*/
+
+function sort(mass) {
+  var temp;
+  for (var i = 0 ; i < (mass.length - 1) / 2 + 1 ; i++) {
+    for (var j = 0 ; j < mass.length - 1 ; j++) {
+      if (mass[j+1] < mass[j]) {
+        temp = mass[j];
+        mass[j] = mass[j+1];
+        mass[j+1] = temp;
+      }
+    }
+  }
+  return mass;
+}
+
+// или встроенная сортировка mass_numb.sort() (вероятно O(n log n))
+
+function findDelNumb(mass) {
+  if (mass[0] != 1) return 1;
+  
+  var delNumber = 0;
+  for (var i = 0 ; i < mass.length - 1 ; i++) {
+    if (mass[i] + 1 != mass[i+1]) {
+      delNumber = mass[i] + 1;
+      break;
+    }
+  }
+  if (delNumber == 0) {
+    return (mass.length + 1);
+  } else {
+    return delNumber;
+  }
+}
+
+// изящный способ
+function findDelNumb2(mass) {
+  var N = mass.length + 1;
+  var summFirstMas = (N + 1) * N / 2;
+  var summSecondMas = 0;
+  for (var i = 0 ; i < mass.length ; i++) {
+    summSecondMas += mass[i]; 
+  }
+  return summFirstMas - summSecondMas;
+}
+
+
+var mass_numb = [2,1,4,5];
+console.log(findDelNumb2(mass_numb));
